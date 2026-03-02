fa63ed57a261df15fecfd118ee7c21c0b3f3102d
ramosirana <mighellramhose.17@gmail.com>
2026-03-02 17:46:30 +0800

    Update main4.py

--- main4.py
+++ main4.py
@@ -1,7 +1,32 @@
 from pyscript import document
+import random
 
-teammates = ["Gonzales", "Ramos", "Funtanilla", "Paz", "Tiongson", "Mendoza", "Santos", "Cruz", "San Juan", "De La Cruz", "Bandong", "Fabro", "Taguiga", "Datu", "Esguerra", "Francisco", "Ibarra", "Clara", "Reyes", "West", "Lilagan", "Belsa", "Mateo", "Santos", "Ramos", "Smith", "Mallari", "Natividad", "Naldoza", "Escarda", "Deray", "Santos", "Francisco", "Damaso", "Salvi", "Irene", "Pelaez", "Jekyll", "Hyde", "Tiago", "Tan", "Del Prado", "Ferrer", "Ligas"]
-bdog = document.GetElementbyId("rb").value
-bbear = document.GetElementbyId("bb").value
-ghornets = document.GetElementbyId("gh").value
-ytigers = document.GetElementbyId("yt").value
+# sample player list
+teammates = [
+"Gonzales", "Ramos", "Funtanilla", "Paz", "Tiongson", "Mendoza", "Santos", "Cruz",
+"San Juan", "De La Cruz", "Bandong", "Fabro", "Taguiga", "Datu", "Esguerra",
+"Francisco", "Ibarra", "Clara", "Reyes", "Lilagan", "Belsa", "Mateo",
+"Smith", "Mallari", "Natividad", "Naldoza", "Escarda",
+"Deray", "Damaso", "Salvi", "Irene", "Pelaez",
+"Tiago", "Tan", "Del Prado", "Ferrer", "Ligas"
+]
+
+def show_players(event=None):
+    selected_team = document.getElementById("grade").value
+    
+    # pick 15 random players
+    chosen_players = random.sample(teammates, 15)
+
+    output = document.getElementById("output")
+
+    # create HTML list
+    html_list = "<h4>" + selected_team.upper() + " players:</h4><ul>"
+    for player in chosen_players:
+        html_list += f"<li>{player}</li>"
+    html_list += "</ul>"
+
+    output.innerHTML = html_list
+
+
+# run when dropdown changes
+document.getElementById("grade").addEventListener("change", show_players)
